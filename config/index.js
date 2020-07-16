3ab0d4fd8f8de51ecb0879e2bd6f0f642b77f925
hridoy43 <hridoy43@gmail.com>
2020-07-16 19:02:48 +0600

    card component added

--- config/index.js
+++ config/index.js
@@ -1,5 +1,2 @@
 export const CLIENT_URL = "https://tb.primex-bd.com/wp/graphql";
-export const SYSTEM_URL = "https://api.apito.io/system/graphql";
-
-
-// http://3.7.155.224:4000/
+export const SYSTEM_URL = "https://tb.primex-bd.com/wp/graphql"; //dev_url
