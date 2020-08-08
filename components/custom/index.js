c9ca49715268e0f0582af76de69d20a47769d778
hridoy43 <hridoy43@gmail.com>
2020-08-08 10:32:25 +0600

    structure change for debugging

--- components/custom/index.js
+++ components/custom/index.js
@@ -1,4 +1,4 @@
-export { Articles } from './Articles'
 export { PostCard } from './PostCard'
+export { Articles } from './Articles'
 export { PostCarousel } from './PostCarousel'
 export { ShowMore } from './ShowMore'
